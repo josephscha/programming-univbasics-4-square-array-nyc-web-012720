68be4764e226a604a17264a08c5b3f5e4e909936
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 01:21:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,4 @@
 
-# def square_array(array)
-#   counter = 0 
-  
-#   while counter < array.length do 
-#     return array[counter] ** 2 
-#     counter += 1
-#   end
-# end
-
 def square_array(array)
   counter = 0 
   new_array = []
